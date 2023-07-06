ffe04aaa9dfa837645c3f1726b57dfd9fe5cf172
Stefan Petrushevski <theztefan@github.com>
2023-07-06 17:16:51 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Setup Dependabot Version Updates by creating an appropriate dependabot.yml configuration file
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
